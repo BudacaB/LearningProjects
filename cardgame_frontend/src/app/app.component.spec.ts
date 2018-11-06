42a64b117ffe042d7957706f5831685b5877c51f
root <root@Bibanu-PC.localdomain>
2018-11-07 01:23:26 +0200

    1st upload

--- cardgame_frontend/src/app/app.component.spec.ts
+++ cardgame_frontend/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'card-game'`, () => {
+  it(`should have as title 'CardGame'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('card-game');
+    expect(app.title).toEqual('CardGame');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to card-game!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to CardGame!');
   });
 });
